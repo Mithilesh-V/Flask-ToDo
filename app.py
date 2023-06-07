390993e12b4c234a3bb2cd17518f16f0e9120217
Mithilesh-V <cv.mithilesh@gmail.com>
2023-06-07 23:19:57 +0530

    todo done

--- app.py
+++ app.py
@@ -1,17 +1,47 @@
 import pandas as pd
-import sqlite3
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,jsonify,url_for,redirect
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+app.app_context().push()
 
-
-def connect_to_db(database):
-    conn = sqlite3.connect(database)
-    return conn
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    complete = db.Column(db.Boolean)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    todo_list = Todo.query.all()
+    return render_template("index.html", todo_list=todo_list)
+
+@app.route("/add", methods=["POST"])
+def add():
+    title = request.form.get("title")
+    new_todo = Todo(title=title, complete=False)
+    db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for("home"))
+
+@app.route("/update/<int:todo_id>")
+def update(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first()
+    todo.complete = not todo.complete
+    db.session.commit()
+    return redirect(url_for("home"))
+
+@app.route("/delete/<int:todo_id>")
+def delete(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for("home"))
+
 
 if __name__ == "__main__":
+    db.create_all()
     app.run()
